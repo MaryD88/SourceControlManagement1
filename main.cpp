e9086fe5f364480f2cf8b37a73f53dbb2f7b901f
MaryD88 <71677713+MaryD88@users.noreply.github.com>
2025-03-23 17:07:57 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Source Code Management 1
+//
+//  Created by Mary Dalton on 3/23/25.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << "Hello, World!\n";
+    return 0;
+}
